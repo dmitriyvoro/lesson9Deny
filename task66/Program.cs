f3fde6492ce8baa25f03439e73dd75592bd855a1
Димка я <  @mail.ru>
2022-10-22 00:33:16 +0300

    summa naturalnih chisel

---
+++ task66/Program.cs
@@ -0,0 +1,14 @@
+﻿ int SummaNumbers(int m, int n)
+{
+    if (m == 0) return (n * (n + 1)) / 2;            
+    else if (n == 0) return (m * (m + 1)) / 2;       
+    else if (m == n) return m;                       
+    else if (m < n) return n + SummaNumbers(m, n - 1); 
+    else return n + SummaNumbers(m, n + 1);            
+}
+
+Console.Write("введите первое число : ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе число = ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"сумма положительных чисел = {SummaNumbers(m, n)}");
