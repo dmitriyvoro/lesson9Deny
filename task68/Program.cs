2c410c06c796cdfa30b272b3af58fa1596aeaef5
Димка я <  @mail.ru>
2022-10-22 00:37:22 +0300

    akkerman task 68

---
+++ task68/Program.cs
@@ -0,0 +1,22 @@
+﻿int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+ 
+ Console.Write("Введите первое число : ");
+int m =  Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Akkerman(m, n));
